ad9182d3a6f243bdcc6a6fbe81a452c555c1a357
jacob-sahl <jacobsahlmueller@gmail.com>
2023-04-30 15:31:55 -0400

    pre-aws deploy

--- main.ts
+++ main.ts
@@ -1,18 +1,11 @@
-import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
-import path from 'path';
 import 'dotenv/config';
-import { Server } from 'socket.io';
+import express, { Request, Response } from 'express';
 import { createServer } from 'http';
+import path from 'path';
+import { Server } from 'socket.io';
 
 const app = express();
-const server = createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST'],
-  },
-});
 
 const env = process.env.NODE_ENV;
 
@@ -32,6 +25,13 @@ app.get('/*', (req: Request, res: Response) => {
 // Express server listening...
 const port = process.env.PORT || 5000;
 
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
 server.listen(port, () => {
   console.log(`Listening on port ${port}...`);
 });
